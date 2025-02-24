aa63bd27723832a898e5c7d4f5794c8e896695b0
Angelina <angelinajanceva@hotmail.com>
2025-02-24 12:54:12 +0100

    add private methods

--- Services/OneDriveService.cs
+++ Services/OneDriveService.cs
@@ -1,19 +1,15 @@
 ﻿using Microsoft.Graph;
 using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.Graph.Models;
-using Microsoft.Graph.Models.Security;
 
 namespace OneDriveFileManagement.Services
 {
     public class OneDriveService
     {
         private readonly GraphServiceClient graphClient;
-        private readonly AuthenticationService authenticationService;
 
         public OneDriveService(AuthenticationService authenticationService)
         {
-            this.authenticationService = authenticationService;
-
             var authProvider = new BaseBearerTokenAuthenticationProvider(new AccessTokenProvider(authenticationService));
             graphClient = new GraphServiceClient(authProvider);
         }
@@ -21,55 +17,21 @@ namespace OneDriveFileManagement.Services
         /// <summary>
         /// Upload a file to OneDrive
         /// </summary>
-        /// <param name="folderName">Simple string</param>
-        /// <param name="fileRelativePath">NameOfFile.fileExtension (e.g. text.txt) </param>
-        /// <returns></returns>
         public async Task UploadFileAsync(string folderName, string fileRelativePath)
         {
             try
             {
-                var drives = await graphClient.Me.Drives.GetAsync();
-                var driveId = drives.Value.First().Id.ToString();
+                var driveId = await GetDriveIdAsync();
+                var folderId = await GetFolderIdAsync(driveId, folderName);
 
-                var folders = await graphClient.Drives[driveId].Items["root"].Children.GetAsync();
-                if (folders.Value.Any(x => x.Name == folderName))
+                // If folder doesn't exist, create it
+                if (folderId == null)
                 {
-
-                    var folderId = folders.Value.First(x => x.Name == folderName).Id;
-                    var filePath = fileRelativePath;
-                    var fullPath = Path.Combine(Directory.GetCurrentDirectory(), filePath);
-
-                    using (var fileStream = new FileStream(fullPath, FileMode.Open))
-                    {
-                        var uploadedFile = graphClient.Drives[driveId].Items[folderId].ItemWithPath(Path.GetFileName(filePath)).Content;
-
-                        await uploadedFile.PutAsync(fileStream);
-                        Console.WriteLine($"File {filePath} uploaded successfully.");
-                    }
-                }
-                else
-                {
-                    var driveItem = new DriveItem
-                    {
-                        Name = folderName,
-                        Folder = new Folder { },
-                    };
-
-                    var addFolder = await graphClient.Drives[driveId].Items["root"].Children.PostAsync(driveItem);
-
-                    var folderId = addFolder.Id;
-                    var filePath = fileRelativePath;
-                    var fullPath = Path.Combine(Directory.GetCurrentDirectory(), filePath);
-
-                    using (var fileStream = new FileStream(fullPath, FileMode.Open))
-                    {
-                        var uploadedFile = graphClient.Drives[driveId].Items[folderId].ItemWithPath(Path.GetFileName(filePath)).Content;
-
-                        await uploadedFile.PutAsync(fileStream);
-                        Console.WriteLine($"File {filePath} uploaded successfully.");
-                    }
+                    folderId = await CreateFolderAsync(driveId, folderName);
                 }
 
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), fileRelativePath);
+                await UploadFileToFolderAsync(driveId, folderId, fullPath);
             }
             catch (Exception ex)
             {
@@ -80,34 +42,35 @@ namespace OneDriveFileManagement.Services
         /// <summary>
         /// Download file from OneDrive to Local folder
         /// </summary>
-        /// <param name="fileName">NameOfFile.fileExtension (e.g. text.txt)</param>
-        /// <param name="folderName">Simple string, it's expected to be valid, existing Name</param>
-        /// <param name="downloadPath">relative path to local folder (Name of the folder)</param>
-        /// <returns></returns>
         public async Task DownloadFileAsync(string fileName, string folderName, string downloadPath)
         {
             try
             {
+                var driveId = await GetDriveIdAsync();
+                var folderId = await GetFolderIdAsync(driveId, folderName);
 
-                downloadPath  = Path.Combine(Directory.GetCurrentDirectory(), downloadPath);
-                var drives = await graphClient.Me.Drives.GetAsync();
-                var driveId = drives.Value.First().Id.ToString();
-                var folders = await graphClient.Drives[driveId].Items["root"].Children.GetAsync();
-                var folderId = folders.Value.First(x => x.Name == folderName).Id;
+                if (folderId == null)
+                {
+                    Console.WriteLine($"Error: Folder {folderName} not found.");
+                    return;
+                }
 
                 var files = await graphClient.Drives[driveId].Items[folderId].Children.GetAsync();
-                var fileId = files.Value.First(x => x.Name == fileName).Id;
+                var file = files.Value.FirstOrDefault(x => x.Name == fileName);
 
-                using (var stream = await graphClient.Drives[driveId].Items[fileId].Content.GetAsync())
+                if (file == null)
                 {
-                    using (var fileStream = new FileStream(downloadPath, FileMode.Create, FileAccess.Write))
-                    {
-                        await stream.CopyToAsync(fileStream);
-                    }
+                    Console.WriteLine($"Error: File {fileName} not found.");
+                    return;
+                }
+
+                using (var stream = await graphClient.Drives[driveId].Items[file.Id].Content.GetAsync())
+                using (var fileStream = new FileStream(downloadPath, FileMode.Create, FileAccess.Write))
+                {
+                    await stream.CopyToAsync(fileStream);
                 }
 
                 Console.WriteLine($"File downloaded to: {downloadPath}");
-            
             }
             catch (Exception ex)
             {
@@ -115,5 +78,41 @@ namespace OneDriveFileManagement.Services
             }
         }
 
+        #region Private Methods
+        private async Task<string> GetDriveIdAsync()
+        {
+            var drives = await graphClient.Me.Drives.GetAsync();
+            return drives.Value.First().Id.ToString();
+        }
+
+        private async Task<string> GetFolderIdAsync(string driveId, string folderName)
+        {
+            var folders = await graphClient.Drives[driveId].Items["root"].Children.GetAsync();
+            var folder = folders.Value.FirstOrDefault(x => x.Name == folderName);
+            return folder?.Id;
+        }
+
+        private async Task<string> CreateFolderAsync(string driveId, string folderName)
+        {
+            var driveItem = new DriveItem
+            {
+                Name = folderName,
+                Folder = new Folder(),
+            };
+
+            var addFolder = await graphClient.Drives[driveId].Items["root"].Children.PostAsync(driveItem);
+            return addFolder.Id;
+        }
+
+        private async Task UploadFileToFolderAsync(string driveId, string folderId, string filePath)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var uploadedFile = graphClient.Drives[driveId].Items[folderId].ItemWithPath(Path.GetFileName(filePath)).Content;
+                await uploadedFile.PutAsync(fileStream);
+            }
+            Console.WriteLine($"File {filePath} uploaded successfully.");
+        }
+        #endregion
     }
 }
